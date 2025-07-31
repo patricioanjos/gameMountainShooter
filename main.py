a067b5846d890895c5e72c8def545493a1b72a4d
Patrício Samuel <patricioanjosjf@gmail.com>
2025-07-31 19:52:28 -0300

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((800, 600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close window
+            quit() # end pygame
