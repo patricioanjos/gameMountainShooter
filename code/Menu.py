c2b79087da9260523e506516af623e517b6f8531
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-06 08:51:49 -0300

    Implementa navegação do menu

--- code/Menu.py
+++ code/Menu.py
@@ -12,6 +12,7 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./assets/Menu.mp3')
         pygame.mixer_music.play(-1)
 
@@ -22,15 +23,36 @@ class Menu:
             self.menu_text(50, True, "Shooter", COLOR_ORANGE, ((WINDOW_WIDTH / 2), 120))
 
             for i in range(len(MENU_OPTIONS)):
-                self.menu_text(20, False, MENU_OPTIONS[i], COLOR_WHITE, ((WINDOW_WIDTH / 2), 200 + 25 * i))
+                if i == menu_option:
+                    self.menu_text(20, False, MENU_OPTIONS[i], COLOR_ORANGE, ((WINDOW_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, False, MENU_OPTIONS[i], COLOR_WHITE, ((WINDOW_WIDTH / 2), 200 + 25 * i))
 
             pygame.display.flip()
 
+            #Check events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     print("Quitting...")
                     pygame.quit()  # Close window
                     quit()  # end pygame
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTIONS) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTIONS) - 1
+
+                    if event.key == pygame.K_RETURN: #Tecla ENTER
+                        return MENU_OPTIONS[menu_option]
+
 
     def menu_text(self, text_size: int, text_bold: bool, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont('comicsans', size=text_size, bold=text_bold)
