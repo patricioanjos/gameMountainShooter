2835a3d3a87de548d93d23c5c679d0f8dbd31d98
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-11 10:49:37 -0300

    adiciona convert_alpha

--- code/Menu.py
+++ code/Menu.py
@@ -8,7 +8,7 @@ from code.Const import WINDOW_WIDTH, COLOR_ORANGE, MENU_OPTIONS, COLOR_WHITE
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./assets/MenuBg.png')
+        self.surf = pygame.image.load('./assets/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
