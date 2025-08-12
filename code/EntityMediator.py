1e0e7d269f2e38a330100fcc14644f71badd0771
Patrício Samuel <patricioanjosjf@gmail.com>
2025-08-12 08:16:43 -0300

    Cria Mediator Pattern

---
+++ code/EntityMediator.py
@@ -0,0 +1,22 @@
+from code.Enemy import Enemy
+from code.Entity import Entity
+
+
+class EntityMediator:
+    @staticmethod
+    def verify_collision(entity_list: list[Entity]):
+        for i in range(len(entity_list)):
+            test_entity = entity_list[i]
+            EntityMediator.__verify_collision_window(test_entity)
+
+    @staticmethod
+    def __verify_collision_window(entity: Entity):
+        if isinstance(entity, Enemy):
+            if entity.rect.right < 0:
+                entity.health = 0
+
+    @staticmethod
+    def verify_health(entity_list: list[Entity]):
+        for entity in entity_list:
+            if entity.health <= 0:
+                entity_list.remove(entity)
